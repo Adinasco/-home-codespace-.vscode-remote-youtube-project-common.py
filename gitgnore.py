5d6228102dcd6d03a58dc47631d4885f9b850d10
Adinasco <5230510104@stu.ahpu.edu.cn>
2024-04-10 02:48:07 +0800

    Add files via upload

---
+++ gitgnore.py
@@ -0,0 +1,18 @@
+CHANNEL_NAME='GM entertaiment'
+
+SEARCH_LIMIT=2
+
+YOUTUBE_VIDEO_SHAREABLE_LINKS=[
+    'https://www.youtube.com/@gmentertainment2882=3'
+]
+
+VIDEO_TITLE_AND_TIME=[
+#('visit to chinese temple located in south wuhu : wuhu ,china, spring ,festival, prayers','2:26'),
+#('GM entertainment: shatta wale JJC official music video';4:30)
+#('sarkodie ft black sherif country side video/GM entertainment','1:52')
+("Black sherif oh paradise /GM entertainment",'3:08'),
+("Bad influence /GM entertainment",'2:12'),
+("PHD graduation by E-photos: fes morocco /GM entertainment",'1:38')
+    
+]
+RANDOM_VIDEO_TIME='02:00'
